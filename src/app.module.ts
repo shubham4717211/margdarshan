8a3675ec2f292ae3a945349b19c2903e6b2aaea4
shubham4717211 <118740802+shubham4717211@users.noreply.github.com>
2023-04-24 13:14:44 +0530

    jwt auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [ MongooseModule.forRoot('mongodb+srv://shubham:123@cluster0.x8dbuw7.mongodb.net/test'),],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://shubham:123@cluster0.x8dbuw7.mongodb.net/test',
+    ),
+    JwtModule.register({
+      secret: 'your-secret-key', // Replace with your own secret key
+      signOptions: { expiresIn: '30d' }, // Token expires after 30 days
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
